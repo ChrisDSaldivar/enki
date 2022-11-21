68f0845139e65145959c49dc07f7769d1c296cb8
Christopher Saldivar <chrisdsaldivar@gmail.com>
2022-11-21 15:46:31 -0600

    Updates login page to use logo

--- src/components/LoginPage.tsx
+++ src/components/LoginPage.tsx
@@ -119,11 +119,7 @@ export default function LoginPage(props: Props) {
         <div className="bg-zinc-900 text-gray-300 flex min-h-screen items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
           <div className="w-full max-w-md space-y-8">
             <div>
-              <img
-                className="mx-auto h-12 w-auto"
-                src="https://tailwindui.com/img/logos/mark.svg?color=cyan&shade=500"
-                alt="Enki"
-              />
+              <img className="mx-auto h-32 w-auto" src={iconFile} alt="Enki" />
               <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-300">
                 Set your auth token
               </h2>
